d9c0cdc3f801732a0a22ea5aa952ba9746213742
Alejandro © <acocac@gmail.com>
2023-10-05 15:36:55 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+requirements = []
+with open("requirements.txt") as f:
+    for line in f:
+        stripped = line.split("#")[0].strip()
+        if len(stripped) > 0:
+            requirements.append(stripped)
+
+setup(
+    name="stardist_seed_pod",
+    version="0.0.1",
+    description="stardist_seedpod_scivision",
+    author="Evangeline Corcoran",
+    author_email="ecorcoran@turing.ac.uk",
+    url="https://github.com/alan-turing-institute/stardist-seed-pod",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.9",
+)
