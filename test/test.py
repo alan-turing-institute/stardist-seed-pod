0458af99e3baf96e4747a17e12cf6b7661cf2860
Alejandro © <acocac@gmail.com>
2023-10-19 16:00:26 +0100

    change channels in 2d

--- test/test.py
+++ test/test.py
@@ -6,7 +6,7 @@ model = stardist_seed()
 
 # test numpy array
 ## create RGB image
-image_2d = np.random.randint(255, size=(50, 50, 900), dtype=np.uint8)
+image_2d = np.random.randint(255, size=(50, 50, 3), dtype=np.uint8)
 image_3d = np.random.randint(255, size=(50, 50, 1), dtype=np.uint8)
 
 ##predict
