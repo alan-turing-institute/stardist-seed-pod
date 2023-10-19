008430a4ace24b06960423c4cad1b225bbebe599
Alejandro © <acocac@gmail.com>
2023-10-19 15:54:37 +0100

    add test file

---
+++ test/test.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+from stardist_seed_pod import stardist_seed
+
+model = stardist_seed()
+
+# test numpy array
+## create RGB image
+image_2d = np.random.randint(255, size=(50, 50, 900), dtype=np.uint8)
+image_3d = np.random.randint(255, size=(50, 50, 1), dtype=np.uint8)
+
+##predict
+model.predict(image_2d)
